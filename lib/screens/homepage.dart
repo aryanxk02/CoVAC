1fd42efc20414143f2021daf05be24ab6d80377b
aryanxk02 <aryan.kenchappagol@gmail.com>
2021-10-16 18:15:15 +0530

    QnA screen and BottomNavBar added

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -11,9 +11,52 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  int _selectedIndex = 0;
+  static const TextStyle optionStyle =
+      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text(
+      'Index 0: Home',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 1: Business',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 2: School',
+      style: optionStyle,
+    ),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.business),
+            label: 'Info',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.school),
+            label: 'QnA',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.blue[800],
+        onTap: _onItemTapped,
+      ),
       body: Container(
         child: SingleChildScrollView(
           child: Column(
@@ -22,68 +65,73 @@ class _HomePageState extends State<HomePage> {
                 height: MediaQuery.of(context).size.height,
                 width: MediaQuery.of(context).size.width,
                 color: Colors.white,
-                child: Column(children: [
-                  Container(
-                    // color: Colors.red,
-                    height: 250,
-                    decoration: BoxDecoration(
-                      image: DecorationImage(
-                        image: AssetImage("assets/homepage.jpg"),
-                        // fit: BoxFit.cover,
+                child: Column(
+                  children: [
+                    Container(
+                      // color: Colors.red,
+                      height: 250,
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          image: AssetImage("assets/homepage.jpg"),
+                          // fit: BoxFit.cover,
+                        ),
                       ),
                     ),
-                  ),
-                  Container(
-                    height: MediaQuery.of(context).size.height - 250,
-                    width: MediaQuery.of(context).size.width,
-                    decoration: const BoxDecoration(
-                        color: Color(0xFFBBDEFB),
-                        borderRadius: BorderRadius.only(
-                            topLeft: Radius.circular(55),
-                            topRight: Radius.circular(55))),
-                    child: Column(
-                      children: [
-                        SizedBox(
-                          height: 100,
-                        ),
-                        Text(
-                          "Search for your nearest \nvaccination centers",
-                          style: TextStyle(
-                              fontSize: 25, fontWeight: FontWeight.bold),
-                          textAlign: TextAlign.center,
-                        ),
-                        SizedBox(
-                          height: 50,
-                        ),
-                        Text(
-                          "Get a preview list of the nearest centers and check availability of vaccination slots",
-                          style: TextStyle(
-                              fontSize: 15, fontWeight: FontWeight.bold),
-                          textAlign: TextAlign.center,
-                        ),
-                        SizedBox(
-                          height: 45,
-                        ),
-                        ElevatedButton(
-                          onPressed: () {
-                            print("Button Pressed!");
-                            Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (context) => SlotSearch()));
-                          },
-                          child: Container(
-                              child: Text("Get Started",
-                                  style: TextStyle(fontSize: 17))),
-                          style: ElevatedButton.styleFrom(
-                            fixedSize: Size(160, 55),
-                            primary: Colors.blueAccent,
+                    Container(
+                      height: MediaQuery.of(context).size.height - 250,
+                      width: MediaQuery.of(context).size.width,
+                      decoration: const BoxDecoration(
+                          color: Color(0xFFBBDEFB),
+                          borderRadius: BorderRadius.only(
+                              topLeft: Radius.circular(55),
+                              topRight: Radius.circular(55))),
+                      child: Column(
+                        children: [
+                          SizedBox(
+                            height: 100,
                           ),
-                        ),
-                      ],
+                          Text(
+                            "Search for your nearest \nvaccination centers",
+                            style: TextStyle(
+                                fontSize: 25, fontWeight: FontWeight.bold),
+                            textAlign: TextAlign.center,
+                          ),
+                          SizedBox(
+                            height: 50,
+                          ),
+                          Text(
+                            "Get a preview list of the nearest centers and check availability of vaccination slots",
+                            style: TextStyle(
+                                fontSize: 15, fontWeight: FontWeight.bold),
+                            textAlign: TextAlign.center,
+                          ),
+                          SizedBox(
+                            height: 45,
+                          ),
+                          ElevatedButton(
+                            onPressed: () {
+                              print("Button Pressed!");
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => SlotSearch()));
+                            },
+                            child: Container(
+                              child: Text(
+                                "Get Started",
+                                style: TextStyle(fontSize: 17),
+                              ),
+                            ),
+                            style: ElevatedButton.styleFrom(
+                              fixedSize: Size(160, 55),
+                              primary: Colors.blueAccent,
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
-                  ),
-                ]),
+                  ],
+                ),
               ),
             ],
           ),
